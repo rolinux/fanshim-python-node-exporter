4332e67fb4bfb4dcffd8b6b38d594ccfc55f59cf
Radu Coroi <rolinux@users.noreply.github.com>
2020-06-08 17:58:10 +0100

    Pull changes from upstream

--- automatic.py
+++ automatic.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 from fanshim import FanShim
+from threading import Lock
+import colorsys
 import psutil
 import argparse
 import time
@@ -8,37 +10,52 @@ import sys
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--threshold', type=float, default=55.0, help='Temperature threshold in degrees C to enable fan')
-parser.add_argument('--hysteresis', type=float, default=5.0, help='Distance from threshold before fan is disabled')
+parser.add_argument('--threshold', type=float, default=-1, help='Temperature threshold in degrees C to enable fan')
+parser.add_argument('--hysteresis', type=float, default=-1, help='Distance from threshold before fan is disabled')
+
+parser.add_argument('--off-threshold', type=float, default=55.0, help='Temperature threshold in degrees C to enable fan')
+parser.add_argument('--on-threshold', type=float, default=65.0, help='Temperature threshold in degrees C to disable fan')
 parser.add_argument('--delay', type=float, default=2.0, help='Delay, in seconds, between temperature readings')
-parser.add_argument('--prom_temp_file', type=str, default='/etc/node_exporter/fanshim_soc_temp.prom', help='Prom file with SoC temperature')
-parser.add_argument('--prom_cpu_freq_file', type=str, default='/etc/node_exporter/fanshim_cpu_freq.prom', help='Prom file with cpu freq')
-parser.add_argument('--prom_fan_enabled_file', type=str, default='/etc/node_exporter/fanshim_fan_enabled.prom', help='Prom file with fan status')
 parser.add_argument('--preempt', action='store_true', default=False, help='Monitor CPU frequency and activate cooling premptively')
 parser.add_argument('--verbose', action='store_true', default=False, help='Output temp and fan status messages')
 parser.add_argument('--nobutton', action='store_true', default=False, help='Disable button input')
 parser.add_argument('--noled', action='store_true', default=False, help='Disable LED control')
+parser.add_argument('--brightness', type=float, default=255.0, help='LED brightness, from 0 to 255')
+parser.add_argument('--prom_temp_file', type=str, default='/etc/node_exporter/fanshim_soc_temp.prom', help='Prom file with SoC temperature')
+parser.add_argument('--prom_cpu_freq_file', type=str, default='/etc/node_exporter/fanshim_cpu_freq.prom', help='Prom file with cpu freq')
+parser.add_argument('--prom_fan_enabled_file', type=str, default='/etc/node_exporter/fanshim_fan_enabled.prom', help='Prom file with fan status')
 
 args = parser.parse_args()
 
 
 def clean_exit(signum, frame):
     set_fan(False)
-    fanshim.set_light(0, 0, 0)
+    if not args.noled:
+        fanshim.set_light(0, 0, 0)
     sys.exit(0)
 
 
-def update_led(state):
-    if args.noled:
-        return
-    if state:
-        fanshim.set_light(0, 255, 0)
-    else:
-        fanshim.set_light(255, 0, 0)
+def update_led_temperature(temp):
+    led_busy.acquire()
+    temp = float(temp)
+    temp -= args.off_threshold
+    temp /= float(args.on_threshold - args.off_threshold)
+    temp = max(0, min(1, temp))
+    temp = 1.0 - temp
+    temp *= 120.0
+    temp /= 360.0
+    r, g, b = [int(c * 255.0) for c in colorsys.hsv_to_rgb(temp, 1.0, args.brightness / 255.0)]
+    fanshim.set_light(r, g, b)
+    led_busy.release()
 
 
 def get_cpu_temp():
-    return psutil.sensors_temperatures()['cpu-thermal'][0].current
+    t = psutil.sensors_temperatures()
+    for x in ['cpu-thermal', 'cpu_thermal']:
+        if x in t:
+            return t[x][0].current
+    print("Warning: Unable to get CPU temperature!")
+    return 0
 
 
 def get_cpu_freq():
@@ -51,7 +68,6 @@ def set_fan(status):
     changed = False
     if status != enabled:
         changed = True
-        update_led(status)
         fanshim.set_fan(status)
     enabled = status
     return changed
@@ -63,12 +79,29 @@ def set_automatic(status):
     last_change = 0
 
 
+if args.threshold > -1 or args.hysteresis > -1:
+    print("""
+The --threshold and --hysteresis parameters have been deprecated.
+Use --on-threshold and --off-threshold instead!
+""")
+    sys.exit(1)
+
+
 fanshim = FanShim()
 fanshim.set_hold_time(1.0)
 fanshim.set_fan(False)
 armed = True
 enabled = False
+led_busy = Lock()
+enable = False
+is_fast = False
 last_change = 0
+signal.signal(signal.SIGTERM, clean_exit)
+
+if args.noled:
+    led_busy.acquire()
+    fanshim.set_light(0, 0, 0)
+    led_busy.release()
 
 t = get_cpu_temp()
 if t >= args.threshold:
@@ -85,23 +118,21 @@ if not args.nobutton:
         elif not armed:
             set_fan(not enabled)
 
-
     @fanshim.on_hold()
     def held_handler():
+        global led_busy
         if args.noled:
-             return
+            return
+        led_busy.acquire()
         for _ in range(3):
             fanshim.set_light(0, 0, 255)
             time.sleep(0.04)
             fanshim.set_light(0, 0, 0)
             time.sleep(0.04)
-            update_led(enabled)
+        led_busy.release()
 
-
-signal.signal(signal.SIGTERM, clean_exit)
 
 try:
-    update_led(fanshim.get_fan())
     while True:
         t = get_cpu_temp()
         with open(args.prom_temp_file, 'w') as prom_temp:
@@ -109,14 +140,25 @@ try:
         f = get_cpu_freq()
         with open(args.prom_cpu_freq_file, 'w') as prom_cpu_freq:
            prom_cpu_freq.write('fanshim_cpu_freq {:.0f}\n'.format(f.current))
+        was_fast = is_fast
+        is_fast = (int(f.current) == int(f.max))
         if args.verbose:
-            print("Current: {:05.02f} Target: {:05.02f} Freq {: 5.02f} Automatic: {} On: {}".format(t, args.threshold, f.current / 1000.0, armed, enabled))
-        if abs(last_change - t) > args.hysteresis and armed:
-            enable = (t >= args.threshold)
-            if args.preempt:
-                enable = enable or (int(f.current) == int(f.max))
+            print("Current: {:05.02f} Target: {:05.02f} Freq {: 5.02f} Automatic: {} On: {}".format(t, args.off_threshold, f.current / 1000.0, armed, enabled))
+
+        if args.preempt and is_fast and was_fast:
+            enable = True
+        elif armed:
+            if t >= args.on_threshold:
+                enable = True
+            elif t <= args.off_threshold:
+                enable = False
+
             if set_fan(enable):
                 last_change = t
+
+        if not args.noled:
+            update_led_temperature(t)
+
         with open(args.prom_fan_enabled_file, 'w') as prom_fan_enabled:
            prom_fan_enabled.write('fanshim_fan_enabled %d\n' % int(enabled))
         time.sleep(args.delay)
